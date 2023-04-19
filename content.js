c5e2c2354fcff45d1c32866f130490c7a9a74d85
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2023-04-19 14:05:51 -0500

    set up primary classes for h1 and p example

--- content.js
+++ content.js
@@ -1,18 +1,23 @@
 // alert("here1");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'TAB_ACTIVATED_D') {
-        alert("TAB_ACTIVATED_D");
+
         document.querySelectorAll("h1").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("p").forEach(item => { item.classList.add("duet-primary") });
+
     } else if (request.message === 'TAB_ACTIVATED_P') {
-        alert("TAB_ACTIVATED_P");
+
         document.querySelectorAll("h1").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("p").forEach(item => { item.classList.add("pro-primary") });
+
     } else if (request.message === 'TAB_ACTIVATED_T') {
-        alert("TAB_ACTIVATED_T");
+
         document.querySelectorAll("h1").forEach(item => { item.classList.add("tri-primary") });
-    } else if (request.message === 'TAB_ACTIVATED_M') {
-        alert("TAB_ACTIVATED_M");
-    } else if (request.message === 'TAB_ACTIVATED_N') {
-        alert("TAB_ACTIVATED_N");
+        document.querySelectorAll("p").forEach(item => { item.classList.add("tri-primary") });
+
+    } else if (request.message === 'TAB_ACTIVATED_M' || request.message === 'TAB_ACTIVATED_N') {
+
         document.querySelectorAll("h1").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("p").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
     }
 });
