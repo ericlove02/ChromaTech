1fa55340827a4d0d9a76651e429d5a0004e06e8a
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2023-04-19 13:54:24 -0500

    message sending working!!!

--- background.js
+++ background.js
@@ -1,48 +1,75 @@
-// function to inject css into visting page
-function injectCSS(tabId, cssFile) {
-    chrome.tabs.insertCSS(tabId, {
-        file: cssFile,
-        runAt: 'document_start'
-    }, function () {
-        console.log('new CSS injected into page, sheet: ' + cssFile);
-    });
-}
-
-chrome.tabs.onActivated.addListener((info) => {
-    chrome.tabs.sendMessage(info.tabId, { message: 'TAB_ACTIVATED' });
-});
-
-// // add classes to elements on page
-
-// // if deut
-// // document . get elemenet by type . add class ( duet )
-// //      in duet.css
-// //       primary-
-// // background-color: xxx
-// // text-color: xxx
-
-// listen for changes to storage data
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    console.log('detected change in storage');
-    // check if key changed
-    if ('colorOption' in changes) {
-        // get new value
-        const newColorblindValue = changes.colorOption.newValue;
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            const tabId = tabs[0].id;
-            console.log('attempting to inject into ' + tabId);
-            if (newColorblindValue === 'deuteranopia') {
-                injectCSS(tabId, 'styles/deuteranopia.css');
-                // add classes to elements
-                document.querySelectorAll("h1").forEach(item => { item.classList.add("duet-primary"); console.log("added class") });
-            } else if (newColorblindValue === 'protanopia') {
-                injectCSS(tabId, 'styles/protanopia.css');
-            } else if (newColorblindValue === 'tritanopia') {
-                injectCSS(tabId, 'styles/tritanopia.css');
-            }
-            else if (newColorblindValue === 'monochromatic') {
-                injectCSS(tabId, 'styles/monochromatic.css');
-            }
+window.onload = async function main() {
+    // function to inject css into visting page
+    function injectCSS(tabId, cssFile) {
+        chrome.tabs.insertCSS(tabId, {
+            file: cssFile,
+            runAt: 'document_start'
+        }, function () {
+            console.log('new CSS injected into page, sheet: ' + cssFile);
         });
     }
-});
+
+    // get data from chrome storage
+    const getStorage = async function (key) {
+        return new Promise((resolve, reject) => {
+            try {
+                chrome.storage.sync.get(key, function (value) {
+                    resolve(value[key]);
+                });
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+    };
+
+    // on a new page load, set classes and inject css
+    chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+        if (changeInfo.status === 'complete') {
+            var option = await getStorage('colorOption');
+            if (option === 'deuteranopia') {
+                chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_D' });
+                injectCSS(tabId, 'styles/deuteranopia.css');
+            } else if (option === 'protanopia') {
+                chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_P' });
+                injectCSS(tabId, 'styles/protanopia.css');
+            } else if (option === 'tritanopia') {
+                chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_T' });
+                injectCSS(tabId, 'styles/tritanopia.css');
+            } else if (option === 'monochromatic') {
+                chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_M' });
+                injectCSS(tabId, 'styles/monochromatic.css');
+            } else {
+                chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_N' });
+            }
+        }
+    });
+
+    // on storage data change, set classes and inject css
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        console.log('detected change in storage');
+        // check if key changed
+        if ('colorOption' in changes) {
+            // get new value
+            const newColorblindValue = changes.colorOption.newValue;
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                const tabId = tabs[0].id;
+                console.log('attempting to inject into ' + tabId);
+                if (newColorblindValue === 'deuteranopia') {
+                    chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_D' });
+                    injectCSS(tabId, 'styles/deuteranopia.css');
+                } else if (newColorblindValue === 'protanopia') {
+                    chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_P' });
+                    injectCSS(tabId, 'styles/protanopia.css');
+                } else if (newColorblindValue === 'tritanopia') {
+                    chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_T' });
+                    injectCSS(tabId, 'styles/tritanopia.css');
+                } else if (newColorblindValue === 'monochromatic') {
+                    chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_M' });
+                    injectCSS(tabId, 'styles/monochromatic.css');
+                } else {
+                    chrome.tabs.sendMessage(tabId, { message: 'TAB_ACTIVATED_N' });
+                }
+            });
+        }
+    });
+}
--- content.js
+++ content.js
@@ -1,9 +1,18 @@
+// alert("here1");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("test");
-    alert("here");
-    if (request.message === 'TAB_ACTIVATED') {
-        console.log("test2");
-        // Perform desired action here
-        alert("here2");
+    if (request.message === 'TAB_ACTIVATED_D') {
+        alert("TAB_ACTIVATED_D");
+        document.querySelectorAll("h1").forEach(item => { item.classList.add("duet-primary") });
+    } else if (request.message === 'TAB_ACTIVATED_P') {
+        alert("TAB_ACTIVATED_P");
+        document.querySelectorAll("h1").forEach(item => { item.classList.add("pro-primary") });
+    } else if (request.message === 'TAB_ACTIVATED_T') {
+        alert("TAB_ACTIVATED_T");
+        document.querySelectorAll("h1").forEach(item => { item.classList.add("tri-primary") });
+    } else if (request.message === 'TAB_ACTIVATED_M') {
+        alert("TAB_ACTIVATED_M");
+    } else if (request.message === 'TAB_ACTIVATED_N') {
+        alert("TAB_ACTIVATED_N");
+        document.querySelectorAll("h1").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
     }
 });
