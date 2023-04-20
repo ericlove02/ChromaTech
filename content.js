8a1ec12aee6b283adc88a1079c1171cb3e3fb0cc
samiyusuf1080 <73402311+samiyusuf1080@users.noreply.github.com>
2023-04-20 18:41:06 -0500

    initial sytling componenets for Deu and Pro

--- content.js
+++ content.js
@@ -3,12 +3,26 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'TAB_ACTIVATED_D') {
 
         document.querySelectorAll("h1").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("h2").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("h3").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("h4").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("h5").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("h6").forEach(item => { item.classList.add("duet-primary") });
         document.querySelectorAll("p").forEach(item => { item.classList.add("duet-primary") });
+        document.querySelectorAll("a").forEach(item => { item.classList.add("duet-link") });
+        document.querySelectorAll("body").forEach(item => { item.classList.add("duet-body") });
 
     } else if (request.message === 'TAB_ACTIVATED_P') {
 
         document.querySelectorAll("h1").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("h2").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("h3").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("h4").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("h5").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("h6").forEach(item => { item.classList.add("pro-primary") });
         document.querySelectorAll("p").forEach(item => { item.classList.add("pro-primary") });
+        document.querySelectorAll("a").forEach(item => { item.classList.add("pro-link") });
+        document.querySelectorAll("body").forEach(item => { item.classList.add("pro-body") });
 
     } else if (request.message === 'TAB_ACTIVATED_T') {
 
@@ -18,6 +32,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     } else if (request.message === 'TAB_ACTIVATED_M' || request.message === 'TAB_ACTIVATED_N') {
 
         document.querySelectorAll("h1").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("h2").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("h3").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("h4").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("h5").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("h6").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
         document.querySelectorAll("p").forEach(item => { item.classList.remove("duet-primary", "pro-primary", "tri-primary") });
+        document.querySelectorAll("a").forEach(item => { item.classList.remove("duet-link", "pro-link", "tri-link") });
+        document.querySelectorAll("body").forEach(item => { item.classList.remove("duet-body", "pro-body", "tri-body") });
     }
 });
